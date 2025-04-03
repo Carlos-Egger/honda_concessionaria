39060f40c211d3f860fd633327d5edbd85a5999a
Pedro Henrique <pedro22070215@aluno.cesupa.br>
2025-04-02 23:15:54 -0300

    remocao .env

--- run.py
+++ run.py
@@ -1,11 +1,3 @@
 from app import create_app
-import os
-app = create_app()
 
-if __name__ == "__main__":
-    if os.name == "nt":  # Se for Windows, usa Waitress
-        from waitress import serve
-        print("Rodando com Waitress no Windows...")
-        serve(app, host="0.0.0.0", port=5000)
-    else:  # Se for Linux (ex: no Render), apenas imprime uma mensagem
-        print("Rodando no ambiente de produção com Gunicorn!")
+app = create_app()
