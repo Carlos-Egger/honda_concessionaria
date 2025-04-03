9b8c9bcb19ae82f796afbacfb531a63e349c5d06
Pedro Henrique <pedro22070215@aluno.cesupa.br>
2025-04-02 23:07:49 -0300

    Procfile

--- run.py
+++ run.py
@@ -1,5 +1,11 @@
 from app import create_app
+import os
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    if os.name == "nt":  # Se for Windows, usa Waitress
+        from waitress import serve
+        print("Rodando com Waitress no Windows...")
+        serve(app, host="0.0.0.0", port=5000)
+    else:  # Se for Linux (ex: no Render), usa Flask normal
+        app.run(host="0.0.0.0", port=5000)
