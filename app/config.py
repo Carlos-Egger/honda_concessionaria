5f56aa686e09f1149a9c1bf66378c09cbb384be0
Pedro Coimbra <--replace-all>
2025-04-03 08:33:14 -0300

    Remove ssl

--- app/config.py
+++ app/config.py
@@ -3,11 +3,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config:
     # Constrói a URI de conexão usando as variáveis de ambiente de forma segura
     # Importante: usamos postgresql+psycopg2 para garantir compatibilidade
     SQLALCHEMY_DATABASE_URI = (
         f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('PASSWORD')}"
-        f"@{os.getenv('HOST')}:{os.getenv('PORT')}/{os.getenv('DATABASE')}?sslmode=require"
+        f"@{os.getenv('HOST')}:{os.getenv('PORT')}/{os.getenv('DATABASE')}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
