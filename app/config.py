c1d3969056de1b2176ff7733c7b7fad8e51288b4
Pedro Henrique <pedro22070215@aluno.cesupa.br>
2025-04-02 23:59:27 -0300

    SSL require

--- app/config.py
+++ app/config.py
@@ -3,13 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-print("Variáveis de ambiente carregadas:")
-print(f"USER: {os.getenv('USER')}")
-print(f"PASSWORD: {os.getenv('PASSWORD')}")
-print(f"HOST: {os.getenv('HOST')}")
-print(f"PORT: {os.getenv('PORT')}")
-print(f"DATABASE: {os.getenv('DATABASE')}")
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('USER')}:{os.getenv('PASSWORD')}@{os.getenv('HOST')}:{os.getenv('PORT')}/{os.getenv('DATABASE')}"
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{os.getenv('USER')}:{os.getenv('PASSWORD')}"
+        f"@{os.getenv('HOST')}:{os.getenv('PORT')}/{os.getenv('DATABASE')}?sslmode=require"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
