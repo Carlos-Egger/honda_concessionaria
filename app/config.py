e08b8174252a14b80f44c1b6d2d399a35c6d0d73
Carlos Egger <carlos.eggern@gmail.com>
2025-06-09 20:51:21 -0300

    refactor

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 class Config:
     # Constrói a URI de conexão usando as variáveis de ambiente de forma segura
     SQLALCHEMY_DATABASE_URI = (
-        f"postgresql://{os.getenv('DB_USER')}:{os.getenv('PASSWORD')}"
+        f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
         f"@{os.getenv('HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DATABASE')}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
