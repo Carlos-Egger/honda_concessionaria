26d6f47bb172249aa3167e8d34836eba5ed501ac
Carlos Egger <carlos.eggern@gmail.com>
2025-06-10 21:55:04 -0300

    adjust password_hash

--- app/models/user.py
+++ app/models/user.py
@@ -4,8 +4,8 @@ from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
-
+    password_hash = db.Column(db.Text, nullable=False)
+    
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
